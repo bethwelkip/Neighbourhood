fac44b28ad8354400d8f71110172ff7f164257c4
Bethwel Kiplimo <bkiplimo@princeton.edu>
2021-04-13 11:07:23 -0400

    add models

--- neighbour/admin.py
+++ neighbour/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from .models import Users, Neighbourhood, Contact, Admin, Business, Post
+from django.contrib.auth.models import User
 
 admin.site.register(Users)
+admin.site.register(Admin)
 admin.site.register(Neighbourhood)
 admin.site.register(Contact)
-admin.site.register(Admin)
 admin.site.register(Business)
 admin.site.register(Post)
 # Register your models here.
